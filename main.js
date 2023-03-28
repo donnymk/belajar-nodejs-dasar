33c9fdb81face4859766a36400770f7c109f986d
Donny Malik Kurniawan <bossdony@gmail.com>
2023-03-28 14:50:03 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+var x = 3;
+var y = 2;
+
+function hitung_hasil_perkalian(a, b){
+    hasil = a * b;
+    return hasil;
+}
+
+console.info("Nilai X: "+x);
+console.info("Nilai Y: "+y);
+console.info("Nilai X dikalikan dengan Y: "+hitung_hasil_perkalian(x ,y));
